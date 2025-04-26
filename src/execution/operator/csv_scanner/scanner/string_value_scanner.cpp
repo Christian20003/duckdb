7b01de05f35ff419413d4076e569056228673afc
Christian <christian-matthias.goellner@stud.uni-bamberg.de>
2025-04-26 10:27:33 +0200

    Remove bug that converting csv values into bfloats does not work correctly

--- src/execution/operator/csv_scanner/scanner/string_value_scanner.cpp
+++ src/execution/operator/csv_scanner/scanner/string_value_scanner.cpp
@@ -289,7 +289,6 @@ void StringValueResult::AddValueToVector(const char *value_ptr, const idx_t size
 		}
 	}
 	bool success = true;
-	float result = 0;
 	switch (parse_types[chunk_col_id].type_id) {
 	case LogicalTypeId::BOOLEAN:
 		success =
@@ -333,10 +332,8 @@ void StringValueResult::AddValueToVector(const char *value_ptr, const idx_t size
 		                          false, state_machine.options.decimal_separator[0]);
 		break;
 	case LogicalTypeId::HALF_FLOAT:
-		success = TryDoubleCast<float>(value_ptr, size, static_cast<float *>(vector_ptr[chunk_col_id])[number_of_rows],
+		success = TryDoubleCast<std::bfloat16_t>(value_ptr, size, static_cast<std::bfloat16_t *>(vector_ptr[chunk_col_id])[number_of_rows],
 		                               false, state_machine.options.decimal_separator[0]);
-		result = static_cast<float *>(vector_ptr[chunk_col_id])[number_of_rows];
-		static_cast<std::bfloat16_t *>(vector_ptr[chunk_col_id])[number_of_rows] = static_cast<std::bfloat16_t>(result);
 		break;
 	case LogicalTypeId::FLOAT:
 		success = TryDoubleCast<float>(value_ptr, size, static_cast<float *>(vector_ptr[chunk_col_id])[number_of_rows],
